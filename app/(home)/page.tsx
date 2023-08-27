1259adabe67cb170d74924621bd3c12f6f0e6bba
Aref <devaref9@gmail.com>
2023-08-27 10:01:15 +0430

    first loading problem solution.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,13 +2,23 @@ import AboutSection from "@/app/(home)/components/AboutSection/AboutSection";
 import BannerSection from "@/app/(home)/components/BannerSection/BannerSection";
 import LatestMoviesSection from "@/app/(home)/components/LatestMoviesSection/LatestMoviesSection";
 import LatestSeriesSection from "@/app/(home)/components/LatestSeriesSection/LatestSeriesSection";
+import { Suspense } from "react";
+import { InfinitySpin } from "react-loader-spinner";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col gap-10">
-      <BannerSection />
-      <LatestMoviesSection />
-      <LatestSeriesSection />
+      <Suspense
+        fallback={
+          <div className="flex justify-center items-center h-screen">
+            <InfinitySpin width="200" color="#f5ad11" />
+          </div>
+        }
+      >
+        <BannerSection />
+        <LatestMoviesSection />
+        <LatestSeriesSection />
+      </Suspense>
       <AboutSection />
     </main>
   );
